16166a639e1bab4e2b9393f19c1286ccc286a068
kmingki <wl1524@naver.com>
2022-06-07 16:30:33 +0900

    image_handler view 삭제

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('create_portfolio', views.create_portfolio),
     path('<int:pk>/portfolio_view_detail/',views.portfolio_view_detail),
     path('projects/create_project',views.create_project),
-    path('projects/image_handler',views.image_handler),
+    #path('projects/image_handler',views.image_handler),
     path('projects/project_view_detail/<int:pk>', views.project_view_detail)
 
 ]
